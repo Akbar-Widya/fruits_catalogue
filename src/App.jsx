0ce8c76db144ca9305b77a2898baa375a1171ad9
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2025-12-08 00:46:21 +0700

    redoing the task: implement the received knowledge about creating & nesting components to the catalogue fruit static page

--- src/App.jsx
+++ src/App.jsx
@@ -17,38 +17,17 @@ const FilterableProductTable = ({ products }) => {
 };
 
 const SearchBar = () => {
-   return (
-      <form action="">
-         <input type="text" name="" placeholder="Search..." id="" />
-         <label>
-            <input type="checkbox" /> Only show products in stock
-         </label>
-      </form>
-   );
+   // return (
+   //    <form action="">
+   //       <input type="text" name="" placeholder="Search..." id="" />
+   //       <label>
+   //          <input type="checkbox" /> Only show products in stock
+   //       </label>
+   //    </form>
+   // );
 };
 
-const ProductTable = ({ products }) => {
-   const rows = [];
-   let lastCategory = null;
-
-   products.forEach((product) => {
-      if (product.category !== lastCategory) {
-         rows.push(
-            <ProductCategoryRow
-               category={product.category}
-               key={product.category}
-            />
-         );
-      }
-      rows.push(
-         <ProductRow
-            product={product}
-            key={product.name}
-         />
-      );
-      lastCategory = product.category;
-   })
-
+const ProductTable = () => {
    return (
       <table>
          <thead>
@@ -57,33 +36,82 @@ const ProductTable = ({ products }) => {
                <th>Price</th>
             </tr>
          </thead>
-         <tbody>{rows}</tbody>
+         <tbody>
+            <ProductRows />
+         </tbody>
       </table>
-   )
+   );
 };
 
-const ProductRow = ({ product }) => {
-   const name = product.stocked ? product.name :
-      <span style={{ color: 'red' }}>
-         {product.name}
-      </span>
-   
-   return (
-      <tr>
-         <td>{name}</td>
-         <td>{product.price}</td>
-      </tr>
-   )
+const ProductRows = () => {
+   const rows = [];
+   const productRows = PRODUCTS;
+   productRows.map((product) => {
+      rows.push(
+         <tr key={product.name}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+         </tr>
+      );
+   });
+   return <>{rows}</>;
 };
+
+// const ProductTable = ({ products }) => {
+//    const rows = [];
+//    let lastCategory = null;
+
+//    products.forEach((product) => {
+//       if (product.category !== lastCategory) {
+//          rows.push(
+//             <ProductCategoryRow
+//                category={product.category}
+//                key={product.category}
+//             />
+//          );
+//       }
+//       rows.push(
+//          <ProductRow
+//             product={product}
+//             key={product.name}
+//          />
+//       );
+//       lastCategory = product.category;
+//    })
+
+//    return (
+//       <table>
+//          <thead>
+//             <tr>
+//                <th>Name</th>
+//                <th>Price</th>
+//             </tr>
+//          </thead>
+//          <tbody>{rows}</tbody>
+//       </table>
+//    )
+// };
+
+// const ProductRow = ({ product }) => {
+//    const name = product.stocked ? product.name :
+//       <span style={{ color: 'red' }}>
+//          {product.name}
+//       </span>
+
+//    return (
+//       <tr>
+//          <td>{name}</td>
+//          <td>{product.price}</td>
+//       </tr>
+//    )
+// };
 
 const ProductCategoryRow = ({ category }) => {
    return (
       <tr>
-         <th colSpan="2">
-            {category}
-         </th>
+         <th colSpan="2">{category}</th>
       </tr>
-   )
+   );
 };
 const App = () => {
    return <FilterableProductTable products={PRODUCTS} />;
